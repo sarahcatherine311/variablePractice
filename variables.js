4b4c89cd45bdef141286b97198886fbdd5c5b765
“Sarah <sarahcatherine311@gmail.com>
2023-01-10 14:15:30 -0700

    Reassign string variables and add console.log statements

--- variables.js
+++ variables.js
@@ -9,3 +9,11 @@ var depositPaid = true
 var collegeEducated = true
 var underAge = false
 
+state = "wyoming"
+email = "email@turing.com"
+
+console.log(state)
+console.log(email)
+console.log(tuitionCost)
+console.log(age)
+
